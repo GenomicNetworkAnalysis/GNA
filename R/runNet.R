a69555903b547773095b9a84a415f90da53f349d
jgthorp <40778973+jgthorp@users.noreply.github.com>
2023-12-06 00:02:22 +1000

    Create runNet.R

---
+++ R/runNet.R
@@ -0,0 +1,56 @@
+.runNet <- function(p_rg,traits,prune="bonf",alpha=0.05,threshold=10,graph_layout="spring"){
+  
+  # prune non significant edges
+  p_rg$weight <- p_rg$Pcor_Estimate
+  
+  if(prune == "bayes"){
+    p_rg$weight[p_rg$BayesFactor10 < threshold] <- 0
+  }
+  if(prune == "bonf"){
+    p_rg$weight[p_rg$Pcor_pvalue >= (0.05/nrow(p_rg))] <- 0
+  }
+  if(prune == "fdr"){
+    p_rg$p_fdr <- p.adjust(p_rg$Pcor_pvalue, method='fdr')
+    p_rg$weight[p_rg$p_fdr >= 0.05] <- 0
+  }
+  if(prune == "alpha"){
+    p_rg$weight[p_rg$Pcor_pvalue >= alpha] <- 0
+  }
+  if(prune == "none"){
+    p_rg$weight <- p_rg$weight
+  }
+  
+  
+  # create weights matrix
+  val <- as.matrix(dcast(p_rg, Trait1 ~ Trait2, value.var = "weight"))
+  rownames(val) <- val[,1]
+  mat <- matrix(nrow = length(traits), ncol = length(traits), dimnames = list(traits,traits))
+  cols <- colnames(mat)[colnames(mat) %in% colnames(val)]
+  rows <- rownames(mat)[rownames(mat) %in% rownames(val)]
+  mat[rows, cols] <- val[rows, cols]
+  mat[lower.tri(mat)] <- t(mat)[lower.tri(mat)]
+  mat <- apply(mat, 2, as.numeric)
+  rownames(mat) <- colnames(mat)
+  
+  
+  # network graph
+  ifelse(graph_layout == "mds",
+         layout <- mds(sim2diss(mat), type = "ordinal")$conf, #multidimensional scaling
+         layout <- graph_layout)
+  
+  network <- qgraph(mat, layout=layout, vsize = 7, threshold = 0, theme = "Borkulo")
+  
+  
+  
+  # centrality metrics
+  centr <- centralityTable(network)
+  centr <- as.data.frame(dcast(centr, node ~ measure, value.var = "value"))
+  centr <- cbind(centr[,"node"],apply(centr[,c("Betweenness","Closeness","Strength","ExpectedInfluence")], 2, as.numeric))
+  colnames(centr)[1] <- "Trait"
+  
+  
+  out <- list(mat,network,centr)
+  names(out) <- c("weights","graph","centrality")
+  return(out)
+
+}
