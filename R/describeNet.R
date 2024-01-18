a2e855912fc7a90bc17a05e6453ce7048ef6b185
jgthorp <40778973+jgthorp@users.noreply.github.com>
2024-01-18 19:30:16 +1000

    Update and rename runNet.R to describeNet.R

---
+++ R/describeNet.R
@@ -0,0 +1,17 @@
+.describeNet <- function(model_out,graph_layout){
+
+  # network graph
+  ifelse(graph_layout == "mds",
+         layout <- mds(sim2diss(mat), type = "ordinal")$conf, #multidimensional scaling
+         layout <- graph_layout)
+  
+  graph <- qgraph(mat, layout=layout, vsize = 7, threshold = 0, theme = "Borkulo")
+  
+  # centrality metrics
+  centr <- centralityTable(network)
+  centr <- as.data.frame(reshape2::dcast(centr, node ~ measure, value.var = "value"))
+  centr <- cbind(centr[,"node"],apply(centr[,c("Betweenness","Closeness","Strength","ExpectedInfluence")], 2, as.numeric))
+  colnames(centr)[1] <- "Trait"
+  
+  return(list(graph=graph,centrality=centr))
+}
