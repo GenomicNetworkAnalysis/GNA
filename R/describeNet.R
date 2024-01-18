7997c9bad818293d679ab89969ac98e34975050f
jgthorp <40778973+jgthorp@users.noreply.github.com>
2024-01-18 20:01:23 +1000

    Update describeNet.R

--- R/describeNet.R
+++ R/describeNet.R
@@ -1,14 +1,14 @@
-.describeNet <- function(model_out,graph_layout){
+.describeNet <- function(omega,graph_layout){
 
   # network graph
   ifelse(graph_layout == "mds",
-         layout <- mds(sim2diss(mat), type = "ordinal")$conf, #multidimensional scaling
+         layout <- mds(sim2diss(omega), type = "ordinal")$conf, #multidimensional scaling
          layout <- graph_layout)
   
-  graph <- qgraph(mat, layout=layout, vsize = 7, threshold = 0, theme = "Borkulo")
+  graph <- qgraph(omega, layout=layout, vsize = 7, threshold = 0, theme = "Borkulo")
   
   # centrality metrics
-  centr <- centralityTable(network)
+  centr <- centralityTable(graph)
   centr <- as.data.frame(reshape2::dcast(centr, node ~ measure, value.var = "value"))
   centr <- cbind(centr[,"node"],apply(centr[,c("Betweenness","Closeness","Strength","ExpectedInfluence")], 2, as.numeric))
   colnames(centr)[1] <- "Trait"
