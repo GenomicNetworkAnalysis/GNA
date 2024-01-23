086548baea13dde6015ae70b336914588c54fda6
agrotzin <33735206+agrotzin@users.noreply.github.com>
2024-01-23 12:01:36 -0700

    Update simNet.R

--- R/simNet.R
+++ R/simNet.R
@@ -1,4 +1,4 @@
-.simNet<-function(covstruc,simruns,trait_pairs){
+.simNet<-function(covstruc,simruns,prune){
   
   print("Beginning estimation of simulated partial correlations to produce power estimates for each edge weight.
         This step may take up to a few hours depending on the number of included traits.")
@@ -14,66 +14,63 @@
   
   #estimate partial correlations for each simnulation
   for(r in 1:simruns){
+    
+    #create empty matrix to store simulated results 
+    simS<-matrix(0, ncol(covstruc$S_LD), ncol(covstruc$S_LD))
+    
+    #store vector for one simulation run in lower triangle
+    simS[lower.tri(simS,diag=TRUE)] <-as.numeric(vectorS[r,])
+    
+    #reflect values above the diagonal
+    simS <- simS + t(simS) - diag(diag(simS)) 
+    
+    #name columns
+    colnames(simS)<-colnames(covstruc$S_LD)
+    
+    ##smooth to near positive definite if simulate Sis non-positive definite
+    if(eigen(simS)$values[nrow(simS)] <= 0){
+      simS<-as.matrix((nearPD(simS, corr = FALSE))$mat)
+    }
+    
+    #pair simulated S matrix with observed V
+    sim_covstruc<-list(V_LD=covstruc$V_LD,S_LD=simS)
+    
+    traits<-colnames(covstruc$S_LD)
+    
+    #estimate the partial correlations (network weights) for simulation
+    p_rg_sim <- .runGGM(sim_covstruc,toler=NULL)
+    
+    #save only the partial correlation part of simulated output
+    p_rg_sim<-p_rg_sim$parameters
+    
+    #remove the variance estimates
+    p_rg_sim<-p_rg_sim %>% filter(row_number() <= n()-k)
+
+    #save the trait pairs as a consistent first three columns across runs
+    if(r == 1){
+      rownames(p_rg_sim)<-paste0(p_rg_sim$trait1, p_rg_sim$op,p_rg_sim$trait2,sep="")
+    }
   
-  #create empty matrix to store simulated results 
-  simS<-matrix(0, ncol(covstruc$S_LD), ncol(covstruc$S_LD))
-  
-  #store vector for one simulation run in lower triangle
-  simS[lower.tri(simS,diag=TRUE)] <-as.numeric(vectorS[r,])
-  
-  #reflect values above the diagonal
-  simS <- simS + t(simS) - diag(diag(simS)) 
-  
-  #name columns
-  colnames(simS)<-colnames(covstruc$S_LD)
-  
-  ##smooth to near positive definite if simulate Sis non-positive definite
-  if(eigen(simS)$values[nrow(simS)] <= 0){
-    simS<-as.matrix((nearPD(simS, corr = FALSE))$mat)
+    #store p-value for each partial correlation for given simulation 
+    simresults[,r]<-p_rg_sim$p
+    
+    print(paste0("Estimated Simulation ", r, sep = " "))
   }
   
-  #pair simulated S matrix with observed V
-  sim_covstruc<-list(V_LD=covstruc$V_LD,S_LD=simS)
-  
-  traits<-colnames(covstruc$S_LD)
-  
-  #estimate the partial correlations (network weights) for simulation
-  p_rg_sim <- data.frame()
-  for (i in 1:nrow(trait_pairs)){
-    y <- trait_pairs[i,]
-    x <- traits[!traits %in% y]
-    model <- paste0(paste0(y, collapse = "+"), "~", paste0(x, collapse = "+"),"
-                    F1=~",y[1],"
-                    F2=~",y[2],"
-                    F1~~F2
-                    ",y[1],"~~0*",y[1],"
-                    ",y[2],"~~0*",y[2])
-    output_sim <- .pcor(sim_covstruc, model)
-    p_rg_sim <- rbind(p_rg_sim,output_sim)
-    p_rg_sim[i,"Trait1"] <- y[1]
-    p_rg_sim[i,"Trait2"] <- y[2]
-  }
-  
-  p_rg_sim$Pcor_pvalue<-2*pnorm(abs(p_rg_sim$Pcor_Estimate/p_rg_sim$Pcor_SE),lower.tail=FALSE) 
-  
-  #save the trait pairs as a consistent first three columns across runs
-  if(r == 1){
-    rownames(p_rg_sim)<-paste0(p_rg_sim$Trait1, p_rg_sim$op,p_rg_sim$Trait2,sep="")
-  }
-  
-  #store p-value for each partial correlation for given simulation 
-  simresults[,r]<-p_rg_sim$Pcor_pvalue
-   
-  print(paste0("Estimated Simulation ", r, sep = " "))
-  }
-  
-  # Set a bonferroni threshold for p-values
+  # Set appropriate threshold for p-values
+  if(prune == "bonf"){
   threshold<-.05/nrow(simresults)
   
   #calculate proportion of runs that were significant for each parameter (i.e., power)
   powerNet<-(apply(simresults, 1, function(row) sum(row < threshold)))/simruns
+  }
+  
+  if(prune == "fdr"){
+    
+  }
+  
+
   
   return(powerNet)
   
 }
- 
