80137e416ccfb055c209b7573b2d79ae2d0fb3e1
agrotzin <33735206+agrotzin@users.noreply.github.com>
2024-01-24 15:27:13 -0700

    Update GeneNet.R

--- R/GeneNet.R
+++ R/GeneNet.R
@@ -18,6 +18,8 @@ GeneNet <- function(covstruc,traits=NULL,fix_omega="full",simruns=100,reestimate
   if(bayes){
   BF10 <- .BayesF(covstruc,model_out)
   model_out$parameters <- merge(model_out$parameters, BF10, by = c("trait1","trait2"), all.x = TRUE)
+  #sort by original ordering in model_out$parameters to ensure appropriate values are appended in code below
+  modelout$parameters<-modelout$parameters[order(free),]
   }
 
   #simulations to estimate power for each partial rg
