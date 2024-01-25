2ce7088cc2301938705bf5a6d6ae221fe26bdd77
agrotzin <33735206+agrotzin@users.noreply.github.com>
2024-01-24 20:03:09 -0700

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,51 @@
+.sandwichSE<-function(Model_Results,V){
+  
+  ### get necessary model matrices / information
+  omega <- getmatrix(Model_Results, "omega") #weight matrix
+  delta <- getmatrix(Model_Results, "delta") #scaling matrix
+  sigma <- getmatrix(Model_Results, "sigma") #mod-implied cov matrix
+  
+  # extra matrices
+  mat <- lapply(Model_Results@extramatrices, as.matrix)
+  L <- mat$L
+  D <- mat$D #duplication matrix
+  Dstar <- mat$Dstar
+  In <- mat$In #k x k identity matrix
+  A <- mat$A
+  
+  ### wls weight matrix from stage 2
+  sigma_inv <- solve(sigma)
+  S2.W <- 0.5 * t(D) %*% kronecker(sigma_inv, sigma_inv) %*% D
+  
+  ### Jacobian matrix of sigma in respect to theta (all model parameters) i.e "Delta" in genomic sem
+  IminOinv <- solve(diag(ncol(omega)) - omega) #inverse of (identify matrix - omega)
+  delta_IminOinv <- delta %*% IminOinv
+  
+  # model derivatives sigma in respect to omega
+  d_sigma_omega <- L %*% (delta_IminOinv %x% delta_IminOinv) %*% Dstar
+  
+  # model derivatives sigma in respect to delta
+  d_sigma_delta <- L %*% ((delta_IminOinv %x% In) + (In %x% delta_IminOinv)) %*% A
+  
+  # full jacobian
+  S2.delt <- cbind(d_sigma_omega, d_sigma_delta)
+  
+  ### sandwich correction
+  #the "bread" part of the sandwich is the naive covariance matrix of parameter estimates that would only be correct if the fit function were correctly specified
+  bread <- solve(t(S2.delt) %*% S2.W %*% S2.delt, tol = toler)
+  
+  #create the "lettuce" part of the sandwich
+  lettuce <- S2.W %*% S2.delt
+  
+  #ohm-hat-theta-tilde is the corrected sampling covariance matrix of the model parameters
+  Ohtt <- bread %*% t(lettuce) %*% V %*% lettuce %*% bread
+  
+  #the lettuce plus inner "meat" (V) of the sandwich adjusts the naive covariance matrix by using the correct sampling covariance matrix of the observed covariance matrix in the computation
+  SE <- as.vector(sqrt(diag(Ohtt)))
+  
+  return(SE)
+}
+
 .covQC <-function(covstruc,traits){ 
   
 ##read in the LD portion of the V (sampling covariance) matrix
