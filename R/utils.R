0bcd360d793ccaecc2e98881d67418eb34405bb8
agrotzin <33735206+agrotzin@users.noreply.github.com>
2024-01-24 19:27:38 -0700

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -91,7 +91,7 @@ return(list(V_LD=V_LD,S_LD=S_LD))
 
 }
 
-                       .get_V_full <- function(k, V_LD, varSNPSE2, V_SNP) {
+.get_V_full <- function(k, V_LD, varSNPSE2, V_SNP) {
     ##create shell of full sampling covariance matrix
     V_Full<-diag(((k+1)*(k+2))/2)
 
@@ -106,22 +106,9 @@ return(list(V_LD=V_LD,S_LD=S_LD))
     return(V_Full)
 }
 
-.get_V_SNP <- function(SE_SNP, I_LD, varSNP, GC, coords, k, i) {
+.get_V_SNP <- function(SE_SNP, I_LD, varSNP, coords, k, i) {
      V_SNP<-diag(k)
-    #loop to add in the GWAS SEs, correct them for univariate and bivariate intercepts, and multiply by SNP variance from reference panel
-    if(GC == "conserv"){
-      for (p in 1:nrow(coords)) {
-        x<-coords[p,1]
-        y<-coords[p,2]
-        if (x != y) {
-          V_SNP[x,y]<-(SE_SNP[i,y]*SE_SNP[i,x]*I_LD[x,y]*I_LD[x,x]*I_LD[y,y]*varSNP[i]^2)}
-        if (x == y) {
-          V_SNP[x,x]<-(SE_SNP[i,x]*I_LD[x,x]*varSNP[i])^2
-        }
-      }
-    }
-
-    if(GC == "standard"){
+  
       for (p in 1:nrow(coords)) {
         x<-coords[p,1]
         y<-coords[p,2]
@@ -131,19 +118,7 @@ return(list(V_LD=V_LD,S_LD=S_LD))
           V_SNP[x,x]<-(SE_SNP[i,x]*sqrt(I_LD[x,x])*varSNP[i])^2
         }
       }
-    }
-
-    if(GC == "none"){
-      for (p in 1:nrow(coords)) {
-        x<-coords[p,1]
-        y<-coords[p,2]
-        if (x != y) {
-          V_SNP[x,y]<-(SE_SNP[i,y]*SE_SNP[i,x]*I_LD[x,y]*varSNP[i]^2)}
-        if (x == y) {
-          V_SNP[x,x]<-(SE_SNP[i,x]*varSNP[i])^2
-        }
-      }
-    }
+  
     return(V_SNP)
 }
 
@@ -159,5 +134,4 @@ return(list(V_LD=V_LD,S_LD=S_LD))
     }
     return(Z_pre)
 }
-
-                       
+            
