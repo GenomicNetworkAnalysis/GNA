f892f19423b35ab2bff71c26d5d59c9b2812901d
jgthorp <40778973+jgthorp@users.noreply.github.com>
2024-02-06 13:08:54 +1000

    Update pruneNet.R

--- R/pruneNet.R
+++ R/pruneNet.R
@@ -1,31 +1,9 @@
-.pruneNet <- function(model_out,prune="bonf",alpha=0.05,threshold=10,bayes,prunepower=FALSE){
+.pruneNet <- function(model_out,prune="fdr",alpha=0.05,prunepower=FALSE){
   
-  par <- model_out$parameters[model_out$parameters$matrix=="omega",]
-  par$weight <- par$est
+  par <- model_out$parameters[model_out$parameters$matrix=="omega",] #pull parameters table
+  par$p_adj <- p.adjust(par$p, method=prune) #adjusted pvals
+  par$weight <- if_else(par$p_adj < alpha, par$est, 0) #prune non-significant edges
   
-  # prune non significant edges
-  if(prune == "bayes"){
-    if(bayes){
-      if(anyNA(par$BayesFactor10)){
-        warning("Bayes factor estimation produced non-finite values. Consider using a different pruning method")
-      }
-      par$weight[par$BayesFactor10 < threshold] <- 0
-    }else{print("You have specified pruning using bayes estimation but did not request bayes factors to be estimated.")
-    }
-  }
-  
-  if(prune == "bonf"){
-    par$weight[par$p >= (0.05/nrow(par))] <- 0
-  }
-  
-  if(prune == "fdr"){
-    par$weight[p.adjust(par$p, method='fdr') >= 0.05] <- 0
-  }
-  
-  if(prune == "alpha"){
-    par$weight[par$p >= alpha] <- 0
-  }
-
   if(prunepower){
    par$weight[par$power < .8] <- 0 
   }
