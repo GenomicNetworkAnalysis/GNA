b2956dbc89da7d92dc5b80a7fbb811b664360145
jgthorp <40778973+jgthorp@users.noreply.github.com>
2024-11-08 17:20:49 +1000

    Create twasNET.R

---
+++ R/twasNET.R
@@ -0,0 +1,5 @@
+### twas wrapper
+twasNet <- function(covstruc,genes,fix_omega="full",toler=NULL,parallel=TRUE,cores=NULL){
+  
+  gwasNet(covstruc,SNPs=genes,fix_omega=fix_omega,toler=toler,TWAS=TRUE,parallel=parallel,cores=cores)
+}
